2bf9902c6615a4e4614b1bd242e3687b6ca44259
Rafael Baena Neto <rafael.baena@gmail.com>
2018-08-26 13:48:40 -0300

    ADICIONADO VIEW W TEMPLATES DE PESSOAS

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, lista_pessoas
 
 app_name = 'core'
 
 urlpatterns = [
     path('', home, name='core_home'),
+    path('pessoas/', lista_pessoas, name='core_lista_pessoas')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Pessoa
 
 
 def home(request):
     context = {'mensagem': 'Olá Mundo'}
     return render(request, 'index.html', context)
+
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    context = {'pessoas': pessoas}
+    return render(request, 'lista_pessoas.html', context)
