0ede9df056993c8f766d6c08a2f5a8db49a1ed3a
Rafael Baena Neto <rafael.baena@gmail.com>
2018-08-26 13:34:44 -0300

    ADICIONADO SISTEMAS DE TEMPLATES

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Olá Mundo')
+    context = {'mensagem': 'Olá Mundo'}
+    return render(request, 'index.html', context)
