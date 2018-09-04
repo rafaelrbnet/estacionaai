72a9e61505ddb8054ae02004948f444bdea3a3a0
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-04 16:02:27 -0300

    MODIFICAÇÕES PARA SERVIR ARQUIVOS ESTATICOS HEROKU

--- estacionaai/wsgi.py
+++ estacionaai/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
+import os
 from dj_static import Cling
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estacionaai.settings')
 
 application = Cling(get_wsgi_application())
