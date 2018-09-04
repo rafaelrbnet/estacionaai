b827f2a35f1cca8f9ab0281ed7d4e0b6f5da4d17
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-04 16:38:20 -0300

    MODIFICAÇÕES PARA SERVIR ARQUIVOS ESTATICOS HEROKU

--- estacionaai/settings.py
+++ estacionaai/settings.py
@@ -138,6 +138,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Auth
 AUTH_USER_MODEL = 'accounts.User'
 
